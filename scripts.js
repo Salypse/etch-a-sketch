ddc79090d5607e62b71ed099c3c088c5ed6554a0
Salypse <84153483+Salypse@users.noreply.github.com>
2025-08-25 13:24:05 -0500

    Create change size feature

--- scripts.js
+++ scripts.js
@@ -1,26 +1,53 @@
-function testRow(size) {
-    const container = document.querySelector(".container")
+//Starts pages on default size of a 16x16 grid
+makeGrid(16)
+
+function makeGrid(size) {
+    const body = document.body
+
+    let newContainer = document.createElement("div")
+    newContainer.className = "container";
+    body.appendChild(newContainer)
 
     for (let i = 0; i < size; i++) {
         const row = document.createElement("div")
         row.className = "row"
-        container.appendChild(row)
+        newContainer.appendChild(row)
         for (let j = 0; j < size; j++){
             const node = document.createElement("div")
             node.className = "node"
             row.appendChild(node)
         }
     }
+    updateNodeListeners()
 }
 
-testRow(16)
-
-
 //changes the color of node cursor is on
-const nodes = document.querySelectorAll(".node")
-
-nodes.forEach((node) => {
-    node.addEventListener("mouseover", () => {
-        node.style.backgroundColor = "black"
+function updateNodeListeners() {
+    let nodes = document.querySelectorAll(".node")
+    nodes.forEach((node) => {
+        node.addEventListener("mouseover", () => {
+            node.style.backgroundColor = "black";
+        })
     })
-})
+}
+
+
+//When change size button pressed prompt user for new size
+const sizeButton = document.querySelector(".size-button")
+sizeButton.addEventListener("click", function(e) {
+    const container = document.querySelector(".container")
+    container.remove()
+
+    let size;
+    
+    while (true) {
+        size = parseInt(prompt("Please choose a size for the grid"));
+        if (isNaN(size) || !Number.isInteger(size) || size <= 0 || size > 100) {
+            alert("Please enter a number between 1 and 100");
+        }
+        else {
+            break;
+        }
+    }
+    makeGrid(size)
+});
